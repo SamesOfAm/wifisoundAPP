65694e14d65a9b2584ea2044b0102bed05289d94
samesofam <3Tunes3Tunes>
2018-01-23 13:07:53 +0100

    working version with 4 sec of delay and 4 hits each scan

--- app/src/main/java/com/example/floriantepelmann/wifitest/MainActivity.java
+++ app/src/main/java/com/example/floriantepelmann/wifitest/MainActivity.java
@@ -13,12 +13,10 @@ import android.os.Handler;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.lang.*;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import android.media.MediaPlayer;
-// import android.os.SystemClock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         wifiInfo = wifiManager.getConnectionInfo();
         registerReceiver(myBroadCastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         wifiManager.startScan();
-        // wifis.add("-" + SystemClock.elapsedRealtime());
         wifis = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, wifis);
         list.setAdapter(adapter);
@@ -47,14 +44,9 @@ public class MainActivity extends AppCompatActivity {
         wifiManager.startScan();
     }
 
-    // public long timeDifference = 0;
-    public static boolean isBetween(int x, int lower, int upper) {
-        return lower <= x && x <= upper;
-    }
-
     public int getMaxNum(List<ScanResult> results) {
         int maxNum = 15;
-        if(results.size() < 15){
+        if(results.size() < maxNum){
           maxNum = results.size();
         }
         return maxNum;
@@ -62,51 +54,50 @@ public class MainActivity extends AppCompatActivity {
 
     public int determineSoundFile(int idCalc){
         double soundVal = Math.ceil(idCalc / 10) + 2131427329;
-        int soundNum = (int) soundVal;
-        return soundNum;
+        return (int) soundVal;
     }
 
     private void playSound(final int soundNum, long startDelay, long interDelay, int vol) {
-        int MAX_VOLUME = 100;
-        final MediaPlayer sound_01 = MediaPlayer.create(this, soundNum);
-        final float volume = (float) (1 - (Math.log(MAX_VOLUME + vol / Math.log(MAX_VOLUME))));
-        sound_01.setVolume(volume, volume);
-        Handler mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
+        int maxVolume = 50;
+        final MediaPlayer sound = MediaPlayer.create(this, soundNum);
+        final float volume = (float) (1 - (Math.log(maxVolume - vol) / Math.log(maxVolume)));
+        sound.setVolume(volume, volume);
+        Handler firstHit = new Handler();
+        firstHit.postDelayed(new Runnable() {
             public void run() {
                 try {
-                    sound_01.start();
+                    sound.start();
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
             }
         }, startDelay);
-        Handler nHandler = new Handler();
-        nHandler.postDelayed(new Runnable() {
+        Handler secondHit = new Handler();
+        secondHit.postDelayed(new Runnable() {
             public void run() {
                 try {
-                    sound_01.start();
+                    sound.start();
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
             }
         }, (startDelay) + (interDelay));
-        Handler oHandler = new Handler();
-        oHandler.postDelayed(new Runnable() {
+        Handler thirdHit = new Handler();
+        thirdHit.postDelayed(new Runnable() {
             public void run() {
                 try {
-                    sound_01.start();
+                    sound.start();
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
             }
         }, (startDelay) + (interDelay) + (interDelay));
-        Handler pHandler = new Handler();
-        pHandler.postDelayed(new Runnable() {
+        Handler fourthHit = new Handler();
+        fourthHit.postDelayed(new Runnable() {
             public void run() {
                 try {
-                    sound_01.start();
-                    sound_01.release();
+                    sound.start();
+                    sound.release();
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
@@ -117,111 +108,25 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             wifis.clear();
-            // timeDifference = SystemClock.elapsedRealtime() - timeDifference;
-            // wifis.add(timeDifference + " ");
-            // timeDifference = SystemClock.elapsedRealtime();
             List<ScanResult> results = wifiManager.getScanResults();
             Collections.sort(results, new ScanResultComparator());
-
             for (int i = 0; i < getMaxNum(results); i++) {
                 String bssid = results.get(i).BSSID;
-                int rssi = results.get(i).level;
-                int vol = rssi + 50;
+                int vol = results.get(i).level + 100;
                 int idCalc = Math.abs(Integer.decode("0x" + bssid.substring(0, 2))
                         - Integer.decode("0x" + bssid.substring(3, 5))
                         + Integer.decode("0x" + bssid.substring(6, 8))
                         - Integer.decode("0x" + bssid.substring(9, 11))
                         + Integer.decode("0x" + bssid.substring(12, 14))
                         - Integer.decode("0x" + bssid.substring(15, 17)));
-                long startDel = (long) Integer.decode("0x" + bssid.substring(12, 14)) * 5;
-                long interDel = (long) Integer.decode("0x" + bssid.substring(15, 17)) * 5;
-
-
+                long startDel = (long) Integer.decode("0x" + bssid.substring(12, 14)) * 7 + 500;
+                long interDel = (long) Integer.decode("0x" + bssid.substring(15, 17)) * 7 + 500;
                 playSound(determineSoundFile(idCalc), startDel, interDel, vol);
-
-                /* if (isBetween(idCalc, 0, 9)) {
-                    playSound(R.raw.sound_01, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 10, 19)) {
-                    playSound(R.raw.sound_02, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 20, 29)) {
-                    playSound(R.raw.sound_03, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 30, 39)) {
-                    playSound(R.raw.sound_04, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 40, 49)) {
-                    playSound(R.raw.sound_05, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 50, 59)) {
-                    playSound(R.raw.sound_06, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 60, 69)) {
-                    playSound(R.raw.sound_09, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 70, 79)) {
-                    playSound(R.raw.sound_10, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 80, 89)) {
-                    playSound(R.raw.sound_11, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 90, 99)) {
-                    playSound(R.raw.sound_12, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 100, 109)) {
-                    playSound(R.raw.sound_13, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 110, 119)) {
-                    playSound(R.raw.sound_14, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 120, 129)) {
-                    playSound(R.raw.sound_15, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 130, 139)) {
-                    playSound(R.raw.sound_16, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 140, 149)) {
-                    playSound(R.raw.sound_17, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 150, 159)) {
-                    playSound(R.raw.sound_18, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 160, 169)) {
-                    playSound(R.raw.sound_20, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 170, 179)) {
-                    playSound(R.raw.sound_21, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 180, 189)) {
-                    playSound(R.raw.sound_22, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 190, 199)) {
-                    playSound(R.raw.sound_23, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 200, 209)) {
-                    playSound(R.raw.sound_24, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 210, 219)) {
-                    playSound(R.raw.sound_25, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 220, 229)) {
-                    playSound(R.raw.sound_26, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 230, 239)) {
-                    playSound(R.raw.sound_27, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 240, 249)) {
-                    playSound(R.raw.sound_28, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 250, 259)) {
-                    playSound(R.raw.sound_29, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 260, 269)) {
-                    playSound(R.raw.sound_30, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 270, 279)) {
-                    playSound(R.raw.sound_31, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 280, 289)) {
-                    playSound(R.raw.sound_32, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 290, 299)) {
-                    playSound(R.raw.sound_33, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 300, 309)) {
-                    playSound(R.raw.sound_35, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 310, 319)) {
-                    playSound(R.raw.sound_36, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 320, 329)) {
-                    playSound(R.raw.sound_37, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 330, 339)) {
-                    playSound(R.raw.sound_38, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 340, 349)) {
-                    playSound(R.raw.sound_39, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 350, 359)) {
-                    playSound(R.raw.sound_40, startDel, interDel, vol);
-                } else if (isBetween(idCalc, 360, 369)) {
-                    playSound(R.raw.sound_41, startDel, interDel, vol);
-                } */
-
-                wifis.add(bssid + " " + vol + " " + determineSoundFile(idCalc));
+                wifis.add("sound_" + (determineSoundFile(idCalc) - 2131427328) + " " + startDel + " " + interDel);
             }
-
             adapter.notifyDataSetChanged();
-
-            Handler qHandler = new Handler();
-            qHandler.postDelayed(new Runnable() {
+            Handler scanDelay = new Handler();
+            scanDelay.postDelayed(new Runnable() {
                 public void run() {
                     try {
                         wifiManager.startScan();
@@ -229,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-            }, 3000);
+            }, 4000);
         }
     }
 }
